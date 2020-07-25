f9ca564202db440a4f69b5b2c54b843cb5f050aa
Ricarr <37304218+Repositorio-Ric@users.noreply.github.com>
2020-07-25 21:35:20 +1000

    Updates

---
+++ precache-manifest.a2233317bcb9a4fa72dbd73661ab6260.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02217afa03ddd51c0a02a969493df6cb",
+    "url": "/jammming/index.html"
+  },
+  {
+    "revision": "0830e3a7ceb035c4137a",
+    "url": "/jammming/static/css/main.c51bc736.chunk.css"
+  },
+  {
+    "revision": "aa218a50c202ed0514a3",
+    "url": "/jammming/static/js/2.6150a741.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jammming/static/js/2.6150a741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0830e3a7ceb035c4137a",
+    "url": "/jammming/static/js/main.c7d1cc5a.chunk.js"
+  },
+  {
+    "revision": "d54c33d011ceab8fe7ea",
+    "url": "/jammming/static/js/runtime-main.af6152f4.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
