02768d61146f3a477438085a4d03a6032f6f9b46
Ricarr <37304218+Repositorio-Ric@users.noreply.github.com>
2020-07-27 18:38:27 +1000

    Hook up Search Bar to other components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
-import Track from '../Track/Track';
+import SearchBar from '../SearchBar/SearchBar';
 
 const searchResult1 = {
   name: 'Agua' ,
@@ -80,12 +80,17 @@ class App extends React.Component {
     trackURIs = this.state.playlistTracks.map(track => track);
   }
 
+  search(term) {
+    console.log(term);
+  }
+
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          {/* <!-- Add a SearchBar component --> */}
+          <SearchBar
+            onSearch={this.search}></SearchBar>
           <div className="App-playlist">
             <SearchResults 
               searchResults={this.state.searchResults} 
